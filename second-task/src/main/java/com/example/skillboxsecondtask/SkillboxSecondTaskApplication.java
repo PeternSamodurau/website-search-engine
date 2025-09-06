04a7da92228261f825ce63ef2b8896d6c28273a3
user <spvrent@mail.ru>
2025-09-06 11:46:21 +0300

    начальные настройки

--- second-task/src/main/java/com/example/skillboxsecondtask/SkillboxSecondTaskApplication.java
+++ second-task/src/main/java/com/example/skillboxsecondtask/SkillboxSecondTaskApplication.java
@@ -1,13 +1,23 @@
 package com.example.skillboxsecondtask;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
 
+@Slf4j
 @SpringBootApplication
 public class SkillboxSecondTaskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkillboxSecondTaskApplication.class, args);
+    }
+    @EventListener
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int port = event.getWebServer().getPort();
+        String url = "http://localhost:" + port;
+        log.info("=================== SkillboxSecondTaskApplication started on URL : {}", url);
     }
 
 }
