b8aa0eb25cd1be32441c0ca494ef743ad0d8b9cd
user <spvrent@mail.ru>
2025-09-29 16:20:00 +0300

    get from repositopy

---
+++ five-app/src/main/java/com/example/booksManagement/BooksManagement.java
@@ -0,0 +1,24 @@
+package com.example.booksManagement;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@SpringBootApplication
+public class BooksManagement {
+
+    public static void main(String[] args) {
+        log.info("My ClassLoader for load classes to Metaspace: {}", BooksManagement.class.getClassLoader());
+        SpringApplication.run(BooksManagement.class, args);
+    }
+
+    @EventListener
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int port = event.getWebServer().getPort();
+        String url = "http://localhost:" + port;
+        log.info("=================== BooksManagement started on URL : {}", url);
+    }
+}
