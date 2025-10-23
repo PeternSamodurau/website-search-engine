cb95fe29171f56b14ace955462140d5127fda7f4
user <spvrent@mail.ru>
2025-10-23 17:08:42 +0300

    change User

--- seven-app/src/main/java/com/example/seven_app/model/User.java
+++ seven-app/src/main/java/com/example/seven_app/model/User.java
@@ -1,25 +1,64 @@
 package com.example.seven_app.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "users") //Это параметр, который уточняет, в какую именно коллекцию нужно сохранять объекты User.
-public class User {
+@Document(collection = "users")
+public class User implements UserDetails {
 
     @Id
     private String id;
 
-    @Indexed(unique = true)
     private String username;
 
-    @Indexed(unique = true)
     private String email;
 
+    private String password;
+
+    @Field("roles")
+    private Set<RoleType> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
